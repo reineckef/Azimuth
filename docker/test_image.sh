1d06fcb57176d758c009700f33bd0a4569bf6de3
Frank Reinecke <frank.reinecke@qiagen.com>
2018-12-04 14:24:55 +0100

    Updated Dockerfile to always pull

--- docker/test_image.sh
+++ docker/test_image.sh
@@ -16,4 +16,24 @@ CCAGAAGTTTGAGCCACAAACCCATGGTCA 0.659245390401
 
 "
 
-time docker run --rm -v $(pwd):/shared ${IMAGE} /Azimuth/azimuth/batch /shared/test_data.tsv
+echo "$(date) Now starting a container, and running jobs on it ..."
+
+THISDIR=$(pwd)
+STARTER="docker run -d -v ${THISDIR}:/shared -t ${IMAGE} /bin/bash"
+
+echo "${STARTER}"
+CONTAINER=$( ${STARTER} )
+
+echo "$(date) Container ID = ${CONTAINER}"
+docker ps | grep ${IMAGE}
+
+echo "Running /Azimuth/azimuth/single.py ACAGCTGATCTCCAGATATGACCATGGGTT 2 0.18"
+time docker exec ${CONTAINER} /Azimuth/azimuth/single.py ACAGCTGATCTCCAGATATGACCATGGGTT 2 0.18
+
+echo "Running /Azimuth/azimuth/batch.py /shared/test_data.tsv"
+time docker exec ${CONTAINER} /Azimuth/azimuth/batch.py /shared/1000guides.tsv
+
+echo "Stopping and removing container ..."
+time docker stop ${CONTAINER} && docker rm ${CONTAINER}
+
+echo "$(date) All done."
